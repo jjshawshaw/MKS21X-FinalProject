fee04b287ddd566706ee194bb49af9800d8c48aa
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2019-01-05 19:38:38 -0500

    set up Tile class

---
+++ Tile.java
@@ -0,0 +1,27 @@
+public class Tile{
+
+	int x, y;
+	String val;
+	int mode;
+
+	public Tile(x,y,value){
+
+	}
+
+	getMode(){
+		return mode;
+	}
+
+	getVal(){
+		return val;
+	}
+
+	setMode(int new){
+		mode = new;
+	}
+
+
+
+
+
+}
