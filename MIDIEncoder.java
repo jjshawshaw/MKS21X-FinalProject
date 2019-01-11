17071e5e7f832406fb36b70f9d635f0c0f684597
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2019-01-10 20:21:57 -0500

    fixed the weird cursor glitch

--- MIDIEncoder.java
+++ MIDIEncoder.java
@@ -74,8 +74,10 @@ public class MIDIEncoder{
       {
 
               if (key.getKind() == Key.Kind.Escape) {
+                      note=false;
+                      terminal.setCursorVisible(false);
                       terminal.exitPrivateMode();
-                      running = false;
+                      complete = true;
               }
               if (key.getKind() == Key.Kind.ArrowUp){
                       if (currenty > 0) currenty--;
@@ -130,19 +132,19 @@ public class MIDIEncoder{
               if(!hasLoaded) {
 				putString(0,0,terminal, "AAAAAAAAAAAAAAAAAAAAAAAAAAWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",Terminal.Color.RED,Terminal.Color.RED,Terminal.Color.RED);
 
-				putString(1,5,terminal, "C ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
-				putString(1,6,terminal, "B ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
-				putString(1,7,terminal, "A#",Terminal.Color.WHITE,Terminal.Color.BLACK,Terminal.Color.RED);
-				putString(1,8,terminal, "A ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
-				putString(1,9,terminal, "G#",Terminal.Color.WHITE,Terminal.Color.BLACK,Terminal.Color.RED);
-				putString(1,10,terminal, "G ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
-				putString(1,11,terminal, "F#",Terminal.Color.WHITE,Terminal.Color.BLACK,Terminal.Color.RED);
-				putString(1,12,terminal, "F ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
-				putString(1,13,terminal, "E ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
-				putString(1,14,terminal, "D#",Terminal.Color.WHITE,Terminal.Color.BLACK,Terminal.Color.RED);
-				putString(1,15,terminal, "D ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
-				putString(1,16,terminal, "C#",Terminal.Color.WHITE,Terminal.Color.BLACK,Terminal.Color.RED);
-				putString(1,17,terminal, "C ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
+				putString(0,5,terminal, "C  ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
+				putString(0,6,terminal, "B  ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
+				putString(0,7,terminal, "A#",Terminal.Color.WHITE,Terminal.Color.BLACK,Terminal.Color.RED);
+				putString(0,8,terminal, "A  ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
+				putString(0,9,terminal, "G#",Terminal.Color.WHITE,Terminal.Color.BLACK,Terminal.Color.RED);
+				putString(0,10,terminal, "G  ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
+				putString(0,11,terminal, "F#",Terminal.Color.WHITE,Terminal.Color.BLACK,Terminal.Color.RED);
+				putString(0,12,terminal, "F  ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
+				putString(0,13,terminal, "E  ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
+				putString(0,14,terminal, "D#",Terminal.Color.WHITE,Terminal.Color.BLACK,Terminal.Color.RED);
+				putString(0,15,terminal, "D  ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
+				putString(0,16,terminal, "C#",Terminal.Color.WHITE,Terminal.Color.BLACK,Terminal.Color.RED);
+				putString(0,17,terminal, "C  ",Terminal.Color.BLACK,Terminal.Color.WHITE,Terminal.Color.RED);
                                 putString(1,3,terminal, "currentx: "+ currentx,Terminal.Color.BLUE, Terminal.Color.WHITE,Terminal.Color.RED);
                                 putString(15,3,terminal, "currenty: "+ currenty,Terminal.Color.BLUE, Terminal.Color.WHITE,Terminal.Color.RED);
 																putString(35,3,terminal, "note: "+ note,Terminal.Color.BLUE, Terminal.Color.WHITE,Terminal.Color.RED);
@@ -158,7 +160,7 @@ public class MIDIEncoder{
       }
       if(mode == 1) {
               if(!hasLoaded) {
-        putString(1,3,terminal, "Not game, just a pause!",Terminal.Color.BLUE,Terminal.Color.RED,Terminal.Color.WHITE);
+                putString(1,3,terminal, "Not game, just a pause!",Terminal.Color.BLUE,Terminal.Color.RED,Terminal.Color.WHITE);
                 hasLoaded = true;
               }
       }
